0d27fb645c3f532885cdac99cd09e178596c703d
Gareth <Gareth.kh.lau@gmail.com>
2021-03-24 23:56:19 -0600

    feat(core): Handle default variant

--- src/components/Selector.tsx
+++ src/components/Selector.tsx
@@ -8,9 +8,12 @@ interface SelectorProps {
 
 export const Selector: React.FC<SelectorProps> = ({ name, children }) => {
   const selection = useSelection(name);
+  const childArray = useMemo(() => React.Children.toArray(children), [
+    children,
+  ]);
 
   const selectedVariant = useMemo(() => {
-    return React.Children.toArray(children).filter((child) => {
+    return childArray.filter((child) => {
       if (
         React.isValidElement<{ name: string }>(child) &&
         child.props.name === selection
@@ -19,15 +22,34 @@ export const Selector: React.FC<SelectorProps> = ({ name, children }) => {
       }
       return false;
     });
-  }, [children, selection]);
+  }, [childArray, selection]);
+
+  const fallback = useMemo(() => {
+    const elems = childArray.filter((child) => {
+      // Check for default element to fallback to
+      if (
+        React.isValidElement<{ default: boolean }>(child) &&
+        !!child.props.default
+      ) {
+        return true;
+      }
+      return false;
+    });
+    if (elems.length === 1) {
+      return elems[0];
+    }
+    return null;
+  }, [childArray]);
 
   if (React.Children.count(children) === 1) {
     return <span>{children}</span>;
-  } else if (!selection) {
-    return (
-      <span selector-name={name}>{React.Children.toArray(children)[0]}</span>
-    );
-  } else {
+  } else if (selection) {
     return <span selector-name={name}>{selectedVariant}</span>;
+  } else if (fallback) {
+    // Fallback to default variant
+    return <span>{fallback}</span>;
+  } else {
+    // No default specified, fallback to first element
+    return <span selector-name={name}>{childArray[0]}</span>;
   }
 };
